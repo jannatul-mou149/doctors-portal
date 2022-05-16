07b4b13a59852b9db1aa45a5264bb4362f337435
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-05-16 21:35:49 +0600

    authentication done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login';
+import Signup from './Pages/Login/Signup';
 import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='about' element={<About></About>}></Route>
         <Route path='appointment' element={<Appointment></Appointment>}></Route>
         <Route path='login' element={<Login></Login>}></Route>
+        <Route path='signup' element={<Signup></Signup>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
